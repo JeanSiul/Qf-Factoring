d2cf1fd6089486ff466e89d72347ed82f891ea7f
JeanSiul <siul.altamirano@gmail.com>
2026-05-11 23:25:42 -0500

    Update embed-token.js

--- api/powerbi/embed-token.js
+++ api/powerbi/embed-token.js
@@ -1,5 +1,5 @@
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   try {
     if (req.method !== 'GET') {
       return res.status(405).json({
